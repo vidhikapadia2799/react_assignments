4451c75751aef356bf9244c6f6a9880e354ffe8c
Vidhi Kapadia <vidhikap27@gmail.com>
2022-02-23 18:54:27 +0530

    commit practical-03

---
+++ Practical_03/src/Constant.js
@@ -0,0 +1,23 @@
+export const Weekday = [
+  "SUNDAY",
+  "MONDAY",
+  "TUESDAY",
+  "WEDNESDAY",
+  "THURSDAY",
+  "FRIDAY",
+  "SATURDAY",
+];
+export const Month = [
+  "JAN",
+  "FEB",
+  "MARCH",
+  "APRIL",
+  "MAY",
+  "JUNE",
+  "JULY",
+  "AUG",
+  "SEPT",
+  "OCT",
+  "NOV",
+  "DEC",
+];
